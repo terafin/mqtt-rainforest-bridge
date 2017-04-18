00cc247278be5e54516792d468e560bc27d0932b
Justin Wood <woody@apple.com>
2017-04-18 01:18:33 -0500

    Removing this

--- homeautomation-js-lib.git/logging.js
+++
@@ -1,9 +0,0 @@
-enable_logging = false
-
-exports.log = function(some_string) {
-    if (enable_logging) console.log(some_string)
-}
-
-exports.set_enabled = function(enabled) {
-    enable_logging = enabled
-}
